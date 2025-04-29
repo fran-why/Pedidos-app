046894c5e79514588a40457e19f2f4b5596c0ed2
Franklin Cardenas <michelizabela123@outlook.com>
2025-04-29 17:14:07 -0500

    ya? por fin terminado ;c

--- Pedidos-app/Pedido.cs
+++ Pedidos-app/Pedido.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pedidos_app
+namespace PedidosApp
 {
-    class Pedido
+    public class Pedido
     {
+        public string Cliente { get; set; }
+        public string Producto { get; set; }
+        public bool Urgente { get; set; }
+        public double Peso { get; set; }
+        public int Distancia { get; set; }
+        public IMetodoEntrega MetodoEntrega { get; set; }
+
+        public Pedido(string cliente, string producto, bool urgente, double peso, int distancia)
+        {
+            Cliente = cliente;
+            Producto = producto;
+            Urgente = urgente;
+            Peso = peso;
+            Distancia = distancia;
+
+            MetodoEntrega = EntregaFactory.CrearEntrega(producto, urgente, peso);
+        }
+
+        public double ObtenerCosto()
+        {
+            return MetodoEntrega.CalcularCosto(Distancia);
+        }
     }
 }
